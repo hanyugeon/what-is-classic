ab196d473b2a5f467592452913311575c2b12c85
hanyugeon <hanyugeon@naver.com>
2022-04-29 01:26:08 +0900

    Remove: 필요없는 컴포넌트들 삭제

--- wic/src/components/Contents.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Contents.scss";
-
-const Contents = () => {
-  return <div className="contents">컨탠츠 부분</div>;
-};
-
-export default Contents;
--- wic/src/components/Template.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Template.scss";
-
-const Template = ({ children }) => {
-  return <div className="template">{children}</div>;
-};
-
-export default Template;
