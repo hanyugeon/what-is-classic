3349bfec417c3037384803811aac2035761d047c
hanyugeon <hanyugeon@naver.com>
2022-04-29 19:06:39 +0900

    feat: 어코디언 메뉴 테스트 및 적용

--- wic/src/components/Chopin.js
+++ wic/src/components/Chopin.js
@@ -1,9 +1,60 @@
 import React from "react";
+import Accordion from "./accordion/Accordion";
+
+const data = [
+  {
+    title: "녹턴",
+    content: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/tSAwZP8e-zQ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    title: "스케르초",
+    content: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/OCUSalQf-jY"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    title: "콘체르토",
+    content: (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/614oSsDS734"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+];
 
 const Chopin = () => {
   return (
     <div>
       <h3>쇼팽</h3>
+      {data.map((item, index) => (
+        <div key={index}>
+          <Accordion title={item.title} content={item.content} />
+        </div>
+      ))}
     </div>
   );
 };
