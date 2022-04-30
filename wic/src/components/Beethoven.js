00b82f86e872498c60148069f8d9b97418d8cfd8
hanyugeon <hanyugeon@naver.com>
2022-04-30 17:29:54 +0900

    Feat: 고전주의 작곡가 작품 추가 완료

--- wic/src/components/Beethoven.js
+++ wic/src/components/Beethoven.js
@@ -1,9 +1,66 @@
 import React from "react";
+import Accordion from "./accordion/Accordion";
+
+const data = [
+  {
+    title: "엘리제를 위하여",
+    content: (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/s71I_EWJk7I"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    ),
+  },
+  {
+    title: "피아노 소나타",
+    content: (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/CEb8brQHcGk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    ),
+  },
+  {
+    title: "피아노 소나타",
+    content: (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/efA1S8hyBms"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    ),
+  },
+];
 
 const Beethoven = () => {
   return (
     <div>
       <h3>베토벤</h3>
+      {data.map((item, index) => (
+        <div key={index}>
+          <Accordion title={item.title} content={item.content} />
+        </div>
+      ))}
     </div>
   );
 };
--- wic/src/components/Mozart.js
+++ wic/src/components/Mozart.js
@@ -1,9 +1,66 @@
 import React from "react";
+import Accordion from "./accordion/Accordion";
+
+const data = [
+  {
+    title: "피아노 소나타",
+    content: (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/1vDxlnJVvW8"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    ),
+  },
+  {
+    title: "피아노 콘체르토",
+    content: (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Hm9SSSD-_eQ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    ),
+  },
+  {
+    title: "교향곡",
+    content: (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/RPWjixqYBFs"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </div>
+    ),
+  },
+];
 
 const Mozart = () => {
   return (
     <div>
       <h3>모차르트</h3>
+      {data.map((item, index) => (
+        <div key={index}>
+          <Accordion title={item.title} content={item.content} />
+        </div>
+      ))}
     </div>
   );
 };
