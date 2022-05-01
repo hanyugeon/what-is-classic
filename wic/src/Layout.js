1a11676e45049114a8ce26d40c1e0c0cc247dd57
hanyugeon <hanyugeon@naver.com>
2022-05-01 18:06:08 +0900

    Design: 레이아웃 스타일링

--- wic/src/Layout.js
+++ wic/src/Layout.js
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const StyledLayout = styled("div")`
+  width: 960px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 3rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  background-color: #ffffff;
+`;
 
 const Layout = () => {
   return (
-    <div>
+    <StyledLayout>
       <Header />
       <Outlet />
-    </div>
+    </StyledLayout>
   );
 };
 
